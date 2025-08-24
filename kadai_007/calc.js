e28c5975c3063b28f7b7355f4a4a496653dbfa56
yuya-1219 <o.yuuya1219@gmail.com>
2025-08-24 10:43:31 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,12 @@
 let num = 15;
 
 if(num % 3 == 0 && num % 5 == 0) {
-  console.log("３と５の倍数です");
+  console.log("3と5の倍数です");
 } else if(num % 5 == 0) {
-  console.log("５の倍数です");
+  console.log("5の倍数です");
 } else if(num % 3 == 0) {
   console.log("3の倍数です");
 } else {
   console.log(num);
+
 }
