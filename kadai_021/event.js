58eb8be6f475bcdda2568e770d519fa2049711cb
yuya-1219 <o.yuuya1219@gmail.com>
2025-08-26 14:30:38 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+  text.textContent = "ボタンをクリックしました"
+  }, 2000);
+});
+
+
